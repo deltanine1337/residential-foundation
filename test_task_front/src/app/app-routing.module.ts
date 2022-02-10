f2af51956c898cfeb60c5f12398fa1d82ed737e2
deltanine1337 <gleb.baranoff1@yandex.ru>
2022-02-11 01:02:34 +0400

    fix front

--- test_task_front/src/app/app-routing.module.ts
+++ test_task_front/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DistrictComponent} from "./components/district/district.component";
+import {HouseComponent} from "./components/house/house.component";
+import {TenantComponent} from "./components/tenant/tenant.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'district', component: DistrictComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- test_task_front/src/app/app.module.ts
+++ test_task_front/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DistrictComponent } from './components/district/district.component';
 import { HouseComponent } from './components/house/house.component';
 import { TenantComponent } from './components/tenant/tenant.component';
+import { HttpClientModule } from "@angular/common/http";
+
+
 
 @NgModule({
   declarations: [
@@ -15,8 +21,11 @@ import { TenantComponent } from './components/tenant/tenant.component';
     TenantComponent
   ],
   imports: [
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
